608c9124eee5a6f0ed7be0ce5878b584fa1699e0
Jobie Gamao <jobiemaygamao@gmail.com>
2021-11-15 20:05:35 +0800

    Initial Commit

---
+++ wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by may on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
